7f78dda09157cba5202f0255c7a1565ac4ae1523
SbuKhoza <sibusisok59@gmail.com>
2024-09-25 16:51:31 +0200

    css update

--- src/App.js
+++ src/App.js
@@ -2,91 +2,58 @@ import './App.css';
 
 function App() {
   return (
-    
-   <div className='container'>
-
-
-    <div className='leftcont'>
-      
-    <div className='left'>
-
-    <img src='logo.png' alt='logo' className='logo' width={100} height={80}></img>
-
-    </div>
-
-    <div className='left1'>
-      <div className='mid'>
-        <h1 className='midtext'>Mid Century <br></br>Morden Furniture</h1>
-        </div> 
-
-        <div className='morden'>
-        <p>Morden high quality and comfortable <br></br>available on our stores</p>
+    <div className='container'>
+      <nav>
+        <div className='logo'>
+        <img src='logo.png' alt='logo'></img>
         </div>
-
-        <div className='btn'>
-        <button id='button'>Discover Now</button>
+        <ul className="nav-links">
+          <li><a href='about' className='ab'>About Us</a></li>
+          <li><a href='furniture' className='ab'>Furniture</a></li>
+          <li><a href='store' className='ab'>Our Stores</a></li>
+          <li><a href='blog' className='ab'>Blog</a></li>
+        </ul>
+      </nav>
+    
+    <div className='maincont'>
+      <div className='leftcont'>
+        <div className='left'></div>
+        <div className='left1'>
+          <div className='mid'>
+            <h1 className='midtext'>Mid Century <br />Modern Furniture</h1>
+          </div>
+          <div className='morden'>
+            <p>Modern high quality and comfortable<br />available in our stores</p>
+          </div>
+          <div className='btn'>
+            <button id='button'>Discover Now</button>
+          </div>
         </div>
-        
+        <div className='left2'>
+          <div className='scroll'>
+            <p className='mscroll'>Scroll down</p>
+            <img src='mouse.png' alt='mouse'></img>
+          </div>
+          <div className='chair1'>
+            <img src='Chaiir1.jpg' alt='chair' className='chaiir1'></img>
+          </div>
+        </div>
       </div>
 
-    <div className='left2'>
-      <div className='scroll'>
-        <p className='mscroll'>Scroll down</p>  
-        <img src='mouse.png' alt='mouse'></img>
-    
+      <div className='centercont'>
+        <div className='cent1'></div>
+        <div className='chair2'>
+          <img src='Chair2.jpg' alt='chair' className='chaiir1'></img>
+        </div>
       </div>
 
-      <div className='chair1'>
-
-        <img src='Chaiir1.jpg' alt='chair' className='chaiir1'></img>
-
-      </div>
-        
-      </div>
-
-    </div>
-    <div className='centercont'>
-    <div className='center'>
-      <div className='uul'>
-      <ul>
-      <li><a href='about'className='ab'>About Us</a></li>
-      <li><a href='furniture'className='ab'>Furniture</a></li>
-        
-      </ul>
+      <div className='rightcont'>
+        <div className='chair3'>
+          <img src='Chair3.jpg' alt='chair' className='chaiir1'></img>
+        </div>
       </div>
     </div>
-
-    <div className='cent1'>
-      
     </div>
-
-    <div className='chair2'>
-    <img src='Chair2.jpg' alt='chair' className='chaiir1'></img>
-
-    </div>
-
-    </div>
-
-    <div className='rightcont'>
-    <div className='right'>
-    
-    <ul>
-      <li><a href='store'className='ab'>Our Stores</a></li>
-      <li><a href='blog'className='ab'>Blog</a></li>
-      <div className='logo'></div>
-
-        
-      </ul>
-    </div>  
-
-    <div className='chair3'>
-    <img src='Chair3.jpg' alt='chair' className='chaiir1'></img>
-      
-    </div>
-
-    </div>
-  
-  </div>
   );
 }
 
---
+++ src/App2.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App2() {
+  return (
+    <div>
+        <nav>
+            
+             <h5>Medora</h5>
+        </nav>
+
+    </div>
+  )
+}
+
+export default App2
